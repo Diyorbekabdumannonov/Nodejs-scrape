2ed1435d5b60f60a2a7e1e1b0462533cc6c662d6
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-05-08 18:01:34 +0500

    updated

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ const url = "https://www.advantour.com/uzbekistan/tours.htm";
 app.route('/').get(async (res, req) => {
     const { data } = await axios.get(url);
     const $ = cheerio.load(data);
-    const listItems = $(".tour-short > div");
+    const listItems = $(".tour-short .col-sm-8")
     const tours = [];
     listItems.each((el) => {
         const tour = {
             name: ''
         };
-        tour.name = $(this).children().find('p').text()
+        tour.name = $('p').text()
         tours.push(tour);
     });
     fs.writeFile("coutries.json", JSON.stringify(tours, null, 2), (err) => {
