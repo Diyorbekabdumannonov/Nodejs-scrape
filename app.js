0b526395327a54f80476a78578edc0a4bdd4f7b4
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-05-08 17:54:14 +0500

    updated

--- app.js
+++ app.js
@@ -1,28 +1,33 @@
-const express = require('express')
-const request = require('request');
-
-var token = "461e4f06dd0540849e70ef248b15babd69e84d8f16f";
-var targetUrl = "https://www.googletagmanager.com/gtag/js?id=G-PC9XNQ6RD3&l=dataLayer&cx=c"
-var encodedUrl = encodeURIComponent(targetUrl);
-
+const axios = require("axios");
+const cheerio = require("cheerio");
+const fs = require("fs");
+const express = require('express');
+const pretty = require("pretty");
 const app = express()
 
-var options = {
-    'method': 'GET',
-    'url': `https://api.scrape.do?token=${token}&url=${encodedUrl}`,
-    'headers': {}
-};
+const url = "https://www.advantour.com/uzbekistan/tours.htm";
 
-app.route('/').get((res, req) => {
-    request(options, function (error, response) {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(response.body);
-            req.end(response.body)
-        }
+
+app.route('/').get(async (res, req) => {
+    const { data } = await axios.get(url);
+    const $ = cheerio.load(data);
+    const listItems = $(".tour-short > div");
+    const tours = [];
+    listItems.each((el) => {
+        const tour = {
+            name: ''
+        };
+        tour.name = $(this).children().find('p').text()
+        tours.push(tour);
     });
+    fs.writeFile("coutries.json", JSON.stringify(tours, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log("Successfully written data to file");
+    });
+    req.end($)
 })
-
 
 app.listen(3000)
